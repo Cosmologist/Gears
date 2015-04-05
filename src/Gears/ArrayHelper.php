797b487642a3a1d97478285d13393ebf7e8f664d
Cosmologist <12389>
2015-04-05 21:00:25 +0300

    ArrayHelper::unsetValue

---
+++ src/Gears/ArrayHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cosmologist\Gears;
+
+class ArrayHelper
+{
+    /**
+     * Unset array items by value
+     *
+     * @see http://stackoverflow.com/a/7225113/663322
+     *
+     * @param array $array Array
+     * @param mixed $value Value
+     *
+     * @return array Array after the items removing
+     */
+    public static function unsetValue(&$array, $value)
+    {
+        if(($key = array_search($value, $array)) !== false) {
+            unset($array[$key]);
+        }
+
+        return $array;
+    }
+}
