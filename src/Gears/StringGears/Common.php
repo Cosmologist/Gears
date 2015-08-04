5f23b3b44e3dfd0b0d212c6019ba522e864b4345
Pavel Levin <Pavel.Levin@alpari.org>
2015-08-04 13:39:17 +0300

    Add endsWith and startsWith functions

--- src/Gears/StringGears/Common.php
+++ src/Gears/StringGears/Common.php
@@ -77,4 +77,46 @@ class Common
 
         return false;
     }
+
+
+    /**
+     * Determine if a given string starts with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     *
+     * @see Illuminate/Support/Str
+     *
+     * @return bool
+     */
+    public static function startsWith($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle)
+        {
+            if ($needle != '' && strpos($haystack, $needle) === 0) return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Determine if a given string ends with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     *
+     * @see Illuminate/Support/Str
+     *
+     * @return bool
+     */
+    public static function endsWith($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle)
+        {
+            if ((string) $needle === substr($haystack, -strlen($needle))) return true;
+        }
+
+        return false;
+    }
 }
