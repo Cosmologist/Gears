343cff371fa5475e9bdb36f38bfb24404b8277ed
cosmologist <mail@example.com>
2019-04-25 16:19:44 +0300

    Enhance CallableType

--- src/Gears/CallableType.php
+++ src/Gears/CallableType.php
@@ -4,10 +4,10 @@ namespace Cosmologist\Gears;
 
 class CallableType
 {
-    const SEPARATOR = '::';
+    protected const SEPARATOR = '::';
 
     /**
-     * Parse callable from string expression.
+     * Parse callable expression.
      *
      * Supported syntax:
      * - 'Foo\Bar::baz' - callable for static class method.
@@ -19,13 +19,57 @@ class CallableType
      */
     public static function parse(string $expression): callable
     {
-        if (!StringType::contains($expression, self::SEPARATOR)) {
-            return $expression;
-        }
+        return self::isFunctionFormat($expression) ? $expression : self::parseComposite($expression);
+    }
 
+    /**
+     * Parse composite (like "Foo::bar") callable expression
+     *
+     * @param string $expression The callable expression.
+     *
+     * @return callable
+     */
+    protected static function parseComposite($expression): callable
+    {
         return [
             StringType::strBefore($expression, self::SEPARATOR),
             StringType::strAfter($expression, self::SEPARATOR)
         ];
+    }
+
+    /**
+     * Is composite expression
+     *
+     * @param string $expression The callable expression
+     *
+     * @return bool
+     */
+    protected static function isCompositeFormat(string $expression): bool
+    {
+        return StringType::contains($expression, self::SEPARATOR);
+    }
+
+    /**
+     * Is function expression
+     *
+     * @param string $expression The callable expression
+     *
+     * @return bool
+     */
+    public static function isFunctionFormat(string $expression): bool
+    {
+        return !self::isCompositeFormat($expression);
+    }
+
+    /**
+     * Validate callable expression
+     *
+     * @param string $expression The callable expression
+     *
+     * @return bool
+     */
+    public static function validate(string $expression): bool
+    {
+        return is_callable(self::parse($expression));
     }
 }
