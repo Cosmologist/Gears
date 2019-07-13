4b48b1eb8bbfe21d3fb97b12e842b7ffd4631b84
cosmologist <mail@example.com>
2019-07-13 04:52:51 +0300

    Add FileSystem::put, fix duplicate separators FileSystem::joinPaths

--- src/Gears/FileSystem.php
+++ src/Gears/FileSystem.php
@@ -43,8 +43,8 @@ class FileSystem
      */
     public static function joinPaths(...$paths): string
     {
-        return str_replace(
-            [self::UNIX_DIRECTORY_SEPARATOR, self::WINDOWS_DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR],
+        return preg_replace(
+            '/[/\\\]+/',
             DIRECTORY_SEPARATOR,
             implode(
                 DIRECTORY_SEPARATOR,
@@ -58,7 +58,7 @@ class FileSystem
      *
      * Replace the separators in the path to the system suitable separators
      *
-     * @param string $path The path
+     * @param string $path      The path
      * @param string $separator The separator [optional]
      *
      * @return string
@@ -66,5 +66,22 @@ class FileSystem
     public static function correctPathSeparators(string $path, string $separator = DIRECTORY_SEPARATOR): string
     {
         return str_replace([self::WINDOWS_DIRECTORY_SEPARATOR, self::UNIX_DIRECTORY_SEPARATOR], $separator, $path);
+    }
+
+    /**
+     * Write a string to a file
+     *
+     * @param string $filename Path to the file where to write the data
+     * @param string $data     The data to write
+     */
+    public static function put(string $filename, string $data = null)
+    {
+        $directory = dirname($filename);
+
+        if (!file_exists($directory) && !mkdir($directory, 0777, true) && !is_dir($directory)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $directory));
+        }
+
+        file_put_contents($filename, $data);
     }
 }
