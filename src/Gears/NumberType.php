60e61c50a5165fa3a0b66c1c4d7b347e213622b7
Cosmologist <foo@bar.com>
2019-12-05 19:33:07 +0300

    Fix incorrect casting for NumberType::parse

--- src/Gears/NumberType.php
+++ src/Gears/NumberType.php
@@ -17,9 +17,8 @@ class NumberType
     public static function parse($value): ?float
     {
         $result = filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION | FILTER_FLAG_ALLOW_THOUSAND | FILTER_FLAG_ALLOW_SCIENTIFIC);
-        $result = (float)$result ?? null;
 
-        return $result;
+        return $result === false ? null : (float) $result;
     }
 
     /**
