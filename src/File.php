ee03cb6430539b943e143bf613b56f531bf837cc
kosmos <sunfundev@gmail.com>
2026-03-18 22:21:12 +0300

    fix

--- src/File.php
+++ src/File.php
@@ -3,6 +3,7 @@
 namespace Cosmologist\Gears;
 
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
